b8975c2d7190b3fa53947b6a71bfe4f12da1e400
Rupps101 <ziraakw@gmail.com>
2025-08-19 14:43:15 +1200

    Testing git changes

--- app/src/main/java/com/autgroup/s2025/w201/todo/MainActivity.kt
+++ app/src/main/java/com/autgroup/s2025/w201/todo/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Update welcome message for test",
+        text = "Update welcome message for sharing git",
         modifier = modifier
     )
 }
