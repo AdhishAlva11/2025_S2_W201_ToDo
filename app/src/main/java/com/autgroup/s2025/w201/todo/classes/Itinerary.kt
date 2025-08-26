17b014dc46103a973228cc33551838ed2a9a9a9c
Oracles3 <64440554+Oracles3@users.noreply.github.com>
2025-08-26 20:44:22 +1200

    Added basic Itinerary data code:
    - itineraryID : String
    - itineraryName : String
    - itineraryDateRange : Date
    - itineraryActivities : List<Activity>

--- app/src/main/java/com/autgroup/s2025/w201/todo/classes/Itinerary.kt
+++ app/src/main/java/com/autgroup/s2025/w201/todo/classes/Itinerary.kt
@@ -1,4 +1,36 @@
 package com.autgroup.s2025.w201.todo.classes
 
-class Itinerary {
+import java.util.Date
+
+class Itinerary (
+    val itineraryID: String,
+    val itineraryName: String,
+    val itineraryDataRange: Date
+)
+{
+    //Reference to a list of Activity objects
+    private val itineraryActivities: MutableList<Activity> = mutableListOf()
+
+    //Add an activity to the itinerary
+    fun addActivity(activity: Activity){
+        itineraryActivities.add(activity)
+    }
+
+    //Remove an activity by its ID
+    fun removeActivity(activityID: String): Boolean {
+        val iterator = itineraryActivities.iterator()
+        while (iterator.hasNext()) {
+            val activity = iterator.next()
+            if (activity.activityID == activityID) {
+                iterator.remove()
+                return true
+            }
+        }
+        return false
+    }
+
+    //Return a copy of the activities list
+    fun getItineraryActivities(): List<Activity> {
+        return itineraryActivities.toList()
+    }
 }
