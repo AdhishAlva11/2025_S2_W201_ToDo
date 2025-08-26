20c0262d15793e23114bb6226ceb06d989d8a429
AdhishAlva11 <qqv5855@aut.ac.nz>
2025-08-26 15:35:18 +1200

    Created a new Navigation Drawer Activity
    Using this for showing the user there fav activity and itinrary

---
+++ app/src/main/java/com/autgroup/s2025/w201/todo/activities/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.autgroup.s2025.w201.todo.activities.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/com/autgroup/s2025/w201/todo/activities/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package com.autgroup.s2025.w201.todo.activities.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
