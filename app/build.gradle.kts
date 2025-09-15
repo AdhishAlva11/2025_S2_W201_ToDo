3012520991bb254846b25254478f6ccf10679fb1
Rupps101 <ziraakw@gmail.com>
2025-09-15 13:41:18 +1200

    fixing dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.places)
+    implementation(libs.play.services.places)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
