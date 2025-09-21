5a28bae2e48c4e9aee4abfdc782786332d4e48eb
Oracles3 <64440554+Oracles3@users.noreply.github.com>
2025-09-21 23:28:50 +1200

    Changes and fixes

--- app/src/main/java/com/autgroup/s2025/w201/todo/activities/FavouritesAdapter.kt
+++ app/src/main/java/com/autgroup/s2025/w201/todo/activities/FavouritesAdapter.kt
@@ -1,23 +1,22 @@
 package com.autgroup.s2025.w201.todo.activities
 
-import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.autgroup.s2025.w201.todo.R
-import com.google.android.gms.maps.model.LatLng
-import com.autgroup.s2025.w201.todo.classes.*
+import com.autgroup.s2025.w201.todo.classes.Favourite
 
 class FavouritesAdapter(
-    private val favourites: List<Favourite>,
-    private val onItemClick: (Favourite) -> Unit
+    private val favourites: List<Favourite>
 ) : RecyclerView.Adapter<FavouritesAdapter.FavouriteViewHolder>() {
 
-    class FavouriteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val title: TextView = itemView.findViewById(R.id.txtFavTitle)
-        val address: TextView = itemView.findViewById(R.id.txtFavAddress)
+    inner class FavouriteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val txtName: TextView = itemView.findViewById(R.id.txtFavTitle)
+        val txtAddress: TextView = itemView.findViewById(R.id.txtFavAddress)
+        val txtStatus: TextView = itemView.findViewById(R.id.txtFavStatus)
+        val txtRating: TextView = itemView.findViewById(R.id.txtFavRating)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavouriteViewHolder {
@@ -28,12 +27,10 @@ class FavouritesAdapter(
 
     override fun onBindViewHolder(holder: FavouriteViewHolder, position: Int) {
         val favourite = favourites[position]
-        holder.title.text = favourite.title
-        holder.address.text = favourite.address
-
-        holder.itemView.setOnClickListener {
-            onItemClick(favourite)  // Pass clicked favourite back to activity
-        }
+        holder.txtName.text = favourite.name
+        holder.txtAddress.text = favourite.address
+        holder.txtStatus.text = favourite.openStatus
+        holder.txtRating.text = favourite.rating?.toString() ?: "N/A"
     }
 
     override fun getItemCount(): Int = favourites.size
--- app/src/main/java/com/autgroup/s2025/w201/todo/classes/Favourite.kt
+++ app/src/main/java/com/autgroup/s2025/w201/todo/classes/Favourite.kt
@@ -1,10 +1,8 @@
 package com.autgroup.s2025.w201.todo.classes
 
 data class Favourite(
-    val title: String = "",
-    val address: String = "",
-    val openHours: String = "",
-    val starRating: Double = 0.0,
-    val lat: Double = 0.0,
-    val lng: Double = 0.0
+    val name: String? = null,
+    val address: String? = null,
+    val openStatus: String? = null,
+    val rating: Double? = null
 )
