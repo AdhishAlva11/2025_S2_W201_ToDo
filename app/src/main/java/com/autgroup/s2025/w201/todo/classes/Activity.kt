c92bcc2e4718445fa6d1038c937660badb4d6c19
Rupps101 <ziraakw@gmail.com>
2025-09-21 17:31:19 +1200

    Modifying code

--- app/src/main/java/com/autgroup/s2025/w201/todo/classes/Activity.kt
+++ app/src/main/java/com/autgroup/s2025/w201/todo/classes/Activity.kt
@@ -1,9 +1,8 @@
 package com.autgroup.s2025.w201.todo.classes
 
-class Activity {
-    val test = 0
-
-    val name  = "Adhish"
-
-    val name1 = " "
-}
+data class Activity(
+    val name: String,
+    val address: String,
+    val rating: Double,
+    val openStatus: String
+)
