15d15a4aedd196b70f6ce9ff6eecc2b5e9e622b4
Rupps101 <ziraakw@gmail.com>
2025-09-22 17:09:27 +1200

    Adding dependencies

--- app/src/main/java/com/autgroup/s2025/w201/todo/activities/DisplayMapActivity.kt
+++ app/src/main/java/com/autgroup/s2025/w201/todo/activities/DisplayMapActivity.kt
@@ -15,6 +15,7 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+//import com.google.android.libraries.places.api.model.Review
 import okhttp3.*
 import org.json.JSONObject
 import java.io.IOException
--- app/src/main/java/com/autgroup/s2025/w201/todo/classes/PlaceInfo.kt
+++ app/src/main/java/com/autgroup/s2025/w201/todo/classes/PlaceInfo.kt
@@ -1,5 +1,6 @@
 package com.autgroup.s2025.w201.todo.classes
 
+import com.google.android.libraries.places.api.model.Review
 import java.io.Serializable
 
 data class PlaceInfo(
