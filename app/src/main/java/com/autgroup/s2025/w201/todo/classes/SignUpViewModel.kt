8369b1732cf2ff95b52e309f4c87dfa27925b208
Rupps101 <ziraakw@gmail.com>
2025-10-13 15:45:24 +1300

    Trying to get unit testing working

--- app/src/main/java/com/autgroup/s2025/w201/todo/classes/SignUpViewModel.kt
+++ app/src/main/java/com/autgroup/s2025/w201/todo/classes/SignUpViewModel.kt
@@ -5,21 +5,21 @@ import com.google.firebase.auth.FirebaseAuth
 class SignUpViewModel(private val firebaseAuth: FirebaseAuth? = null) {
 
     fun validate(email: String?, password: String?, confirmPassword: String?): String? {
-        /*if (email.isNullOrBlank()) return "Email cannot be empty"
+        if (email.isNullOrBlank()) return "Email cannot be empty"
         if (!isValidEmail(email)) return "Invalid email format"
         if (password.isNullOrBlank()) return "Password cannot be empty"
         if (password.length < 6) return "Password must be at least 6 characters"
-        if (password != confirmPassword) return "Passwords do not match"*/
+        if (password != confirmPassword) return "Passwords do not match"
         return null
     }
 
     private fun isValidEmail(email: String): Boolean {
 
-        //return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
         return false
     }
 
     fun registerWithFirebase(email: String, password: String) {
-        //firebaseAuth?.createUserWithEmailAndPassword(email, password)
+        firebaseAuth?.createUserWithEmailAndPassword(email, password)
     }
 }
